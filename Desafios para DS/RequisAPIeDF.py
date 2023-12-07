8609d19701c6a5cf4576fd80b3628d2714e1ca1b
AudreyMarx <audreymarques87@gmail.com>
2023-12-07 14:43:37 -0300

    update file

--- Desafios para DS/RequisAPIeDF.py
+++ Desafios para DS/RequisAPIeDF.py
@@ -2,15 +2,11 @@ import requests
 import pandas as pd
 import json
 
-#response = requests.get('https://dummyjson.com/comments?limit=10&skip=10&select=body,postId')
 url = 'https://dummyjson.com/comments?limit=10&skip=10&select=body,postId'
 response =  requests.get(url)
 if response.status_code == 200:
     dados = json.loads(response.text)
     df = pd.json_normalize(dados, 'comments')
-    df = df.add_prefix('comments_')
-    #df = pd.DataFrame(dados)
     print(df)
 else:
     print(f"Erro ao obter dados da API. Código de status: {response.status_code}")
-

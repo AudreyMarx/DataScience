93db9468ae2ff4a5c00d6ef7097941d9eed4061a
AudreyMarx <audreymarques87@gmail.com>
2023-12-07 14:41:55 -0300

    update file

--- Desafios para DS/RequisAPIeDF.py
+++ Desafios para DS/RequisAPIeDF.py
@@ -13,12 +13,4 @@ if response.status_code == 200:
     print(df)
 else:
     print(f"Erro ao obter dados da API. Código de status: {response.status_code}")
-"""dados = response.json()
-#df = json.loads(dados)
-df1 = pd.DataFrame(dados)
-df2 = df1.explode('comments')
-df3 = pd.DataFrame(df2, columns=['id', 'body'])
 
-print(df1)
-print(df2)
-print(df3)"""
